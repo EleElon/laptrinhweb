41cbb465d3af6f3d9228c8bf59f831a01395fcff
eleelon <eleelon2309@gmail.com>
2024-05-02 22:07:27 +0700

    lab7

---
+++ example-app/storage/framework/views/cf00d578d790cc8c976f8c44911d7b03.php
@@ -0,0 +1,31 @@
+
+
+
+<?php $__env->startSection('content'); ?>
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>soThich</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $messi->id; ?></td>
+                            <td><?php echo $messi->name; ?></td>
+                            <td><?php echo $messi->email; ?></td>
+                            <td><?php echo $messi->soThich; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\GitHub\laptrinhweb\example-app\resources\views/crud_user/read.blade.php ENDPATH**/ ?>
