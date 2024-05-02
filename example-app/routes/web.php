b61114e7fbf4457ab194082faafc9d0ebf847573
eleelon <eleelon2309@gmail.com>
2024-05-02 22:21:49 +0700

    lab7

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -22,6 +22,7 @@ Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUs
 Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
 Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
 
+//Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
 Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
 
 Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.deleteUser');
@@ -32,6 +33,10 @@ Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user
 Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 
 Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
+
+Route::get('listpost', [CrudUserController::class, 'listpost'])->name('listpost');
+
+Route::get('listFavorities', [CrudUserController::class, 'listFavorities'])->name('listFavorities');
 
 Route::get('/', function () {
     return view('welcome');
